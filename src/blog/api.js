179fbc56c890da848448f6f450be485cc0a5eda2
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-24 18:02:28 +0200

    Update api.js

--- src/blog/api.js
+++ src/blog/api.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api",
-  headers: { "Content-Type": "application/json" }
+export const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
 });
 
-export function setAuthToken(token) {
+export function setToken(token) {
   if (token) api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   else delete api.defaults.headers.common["Authorization"];
 }
-
-export default api;
