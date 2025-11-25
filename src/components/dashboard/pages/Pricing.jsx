fef878d1d024272f44818b0446b05b847057c541
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-25 07:17:06 +0200

    Update Pricing.jsx

--- src/components/dashboard/pages/Pricing.jsx
+++ src/components/dashboard/pages/Pricing.jsx
@@ -5,7 +5,7 @@ export default function Pricing() {
     <div className="p-6 text-gray-900 dark:text-white">
       <h1 className="text-3xl font-bold mb-4">SwiftMeta</h1>
       <p className="text-gray-600 dark:text-gray-400">
-        <
+        
       
       </p>
     </div>
