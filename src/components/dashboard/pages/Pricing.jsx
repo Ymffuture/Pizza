059aa3d78241aa3e194d7659a8edf2307824e712
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-25 07:03:37 +0200

    Update Pricing.jsx

--- src/components/dashboard/pages/Pricing.jsx
+++ src/components/dashboard/pages/Pricing.jsx
@@ -1,11 +1,12 @@
-import BlogPages from "../../NavBlog";
+//import BlogPages from "../../NavBlog";
 
 export default function Pricing() {
   return (
     <div className="p-6 text-gray-900 dark:text-white">
       <h1 className="text-3xl font-bold mb-4">SwiftMeta</h1>
       <p className="text-gray-600 dark:text-gray-400">
-        <BlogPages/>
+        <
+      
       </p>
     </div>
   );
