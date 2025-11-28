59d576a96b1ba7790e704eb7bc67e6242f4f158c
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-28 03:54:16 +0200

    Update GeminiAssistant.jsx

--- src/layouts/GeminiAssistant.jsx
+++ src/layouts/GeminiAssistant.jsx
@@ -6,7 +6,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { coldarkCold } from "react-syntax-highlighter/dist/esm/styles/prism";
-import { Toaster, toast} from "react-hot-toast";
+import { toast} from "react-hot-toast";
 const { TextArea } = Input;
 
 const GeminiAssistant = () => {
