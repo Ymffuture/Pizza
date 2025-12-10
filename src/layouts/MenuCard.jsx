80fdaf8608dfb0c47fbe682222efe888ade05424
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-12-10 10:51:55 +0200

    Update MenuCard.jsx

--- src/layouts/MenuCard.jsx
+++ src/layouts/MenuCard.jsx
@@ -11,7 +11,7 @@ const MenuCard = ({ name, price, tag, image }) => {
           className="w-full h-56 sm:h-64 object-cover group-hover:scale-105 transition-transform duration-300"
         />
         {tag && (
-          <span className="absolute top-3 left-3 bg-blue-500 text-white px-3 py-1 rounded-full text-xs font-medium">
+          <span className="absolute top-3 left-3 bg-red-100 text-white px-3 py-1 rounded-full text-xs font-medium">
             {tag}
           </span>
         )}
@@ -19,7 +19,7 @@ const MenuCard = ({ name, price, tag, image }) => {
 
       {/* Content */}
       <div className="p-5">
-        <h3 className="text-lg sm:text-xl font-semibold text-gray-900 dark:text-gray-100 mb-1 group-hover:text-blue-500 transition-colors duration-300">
+        <h3 className="text-lg sm:text-xl font-semibold text-gray-900 dark:text-gray-700 mb-1 group-hover:text-blue-500 transition-colors duration-300">
           {name}
         </h3>
         <p className="text-gray-600 dark:text-gray-400 font-bold text-md sm:text-lg group-hover:text-gray-200 transition-colors duration-300">
@@ -28,10 +28,10 @@ const MenuCard = ({ name, price, tag, image }) => {
 
         {/* Optional action buttons (like Facebook) */}
         <div className="flex justify-between items-center mt-4">
-          <button className="text-blue-500 hover:underline text-sm font-medium">
+          <button className="text-blue-500 hover:underline text-sm font-medium dark:bg-gray-500 rounded p-2">
             Email Us
           </button>
-          <button className="text-gray-500 dark:text-gray-400 hover:text-blue-500 text-sm font-medium">
+          <button className="text-gray-500 dark:text-gray-400 hover:text-blue-500 text-sm font-medium border">
             Chat
           </button>
         </div>
