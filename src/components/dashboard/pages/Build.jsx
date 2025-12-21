aea8ca2f7750c26568f27db8d64b5f8423f7d54d
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-12-21 10:30:51 +0200

    Update Build.jsx

--- src/components/dashboard/pages/Build.jsx
+++ src/components/dashboard/pages/Build.jsx
@@ -58,7 +58,7 @@ export default function Build() {
   </head>
   <body class="bg-gray-50 text-gray-900">
 ${html}
-
+${bridge} 
 <script>
     document.addEventListener("DOMContentLoaded", () => {
       try {
@@ -68,7 +68,7 @@ ${html}
       }
     });
   </script> 
- ${bridge} 
+
   </body>
 </html>
       `;
