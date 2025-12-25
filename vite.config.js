0f87326313daf4e5b61fc2e33b492452f32c78a2
Kgomotso Leonard Nkosi <futurekgomotso@gmail.com>
2025-12-25 16:20:28 +0100

    123

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-})
+  plugins: [
+    tailwindcss(),
+    react(),
+  ],
+
+  server: {
+    watch: {
+      usePolling: true, // ✅ fixes EPERM / file lock issues on Windows
+    },
+  },
+});
