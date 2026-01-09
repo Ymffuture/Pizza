f7d1adef5e7602284cadb7a78c89ffbd0ebda17a
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-09 06:09:15 +0200

    Update ticketApi.js

--- src/api/ticketApi.js
+++ src/api/ticketApi.js
@@ -1,18 +1,43 @@
+import axios from "axios";
+
 const API = "https://swiftmeta.onrender.com/api/tickets";
 
-export const createTicket = (data) =>
-  fetch(API, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  }).then(res => res.json());
+/* -----------------------------
+   Create ticket
+------------------------------ */
+export const createTicket = async data => {
+  const res = await axios.post(API, data);
+  return res.data;
+};
 
-export const getTicket = (id) =>
-  fetch(`${API}/${id}`).then(res => res.json());
+/* -----------------------------
+   Get single ticket by ID
+------------------------------ */
+export const getTicket = async id => {
+  const res = await axios.get(`${API}/${id}`);
+  return res.data;
+};
 
-export const replyTicket = (id, data) =>
-  fetch(`${API}/${id}/reply`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  }).then(res => res.json());
+/* -----------------------------
+   Reply to ticket
+------------------------------ */
+export const replyTicket = async (id, data) => {
+  const res = await axios.post(`${API}/${id}/reply`, data);
+  return res.data;
+};
+
+/* -----------------------------
+   NEW: Get all tickets (admin)
+------------------------------ */
+export const getAllTickets = async () => {
+  const res = await axios.get(API);
+  return res.data;
+};
+
+/* -----------------------------
+   NEW: Close ticket
+------------------------------ */
+export const closeTicket = async id => {
+  const res = await axios.patch(`${API}/${id}/close`);
+  return res.data;
+};
