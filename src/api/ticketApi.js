654f5f62aa307c217a6c27d636145cc7189222be
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-10 08:21:53 +0200

    Update ticketApi.js

--- src/api/ticketApi.js
+++ src/api/ticketApi.js
@@ -2,41 +2,21 @@ import axios from "axios";
 
 const API = "https://swiftmeta.onrender.com/api/tickets";
 
-/* -----------------------------
-   Create ticket
------------------------------- */
-export const createTicket = async data => {
-  const res = await axios.post(API, data);
-  return res.data;
-};
-
-/* -----------------------------
-   Get single ticket by ID
------------------------------- */
-export const getTicket = async id => {
-  const res = await axios.get(`${API}/${id}`);
-  return res.data;
-};
-
-/* -----------------------------
-   Reply to ticket
------------------------------- */
-export const replyTicket = async (id, data) => {
-  const res = await axios.post(`${API}/${id}/reply`, data);
-  return res.data;
-};
-
-/* -----------------------------
-   NEW: Get all tickets (admin)
------------------------------- */
 export const getAllTickets = async () => {
   const res = await axios.get(API);
   return res.data;
 };
 
-/* -----------------------------
-   NEW: Close ticket
------------------------------- */
+export const getTicket = async id => {
+  const res = await axios.get(`${API}/${id}`);
+  return res.data;
+};
+
+export const replyTicket = async (id, data) => {
+  const res = await axios.post(`${API}/${id}/reply`, data);
+  return res.data;
+};
+
 export const closeTicket = async id => {
   const res = await axios.patch(`${API}/${id}/close`);
   return res.data;
