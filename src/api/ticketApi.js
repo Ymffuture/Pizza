0a8ef9469f329da7e5f950a70a2f328239654561
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-10 08:28:33 +0200

    Update ticketApi.js

--- src/api/ticketApi.js
+++ src/api/ticketApi.js
@@ -2,21 +2,41 @@ import axios from "axios";
 
 const API = "https://swiftmeta.onrender.com/api/tickets";
 
-export const getAllTickets = async () => {
-  const res = await axios.get(API);
+/* -----------------------------
+   Create ticket (USER)
+------------------------------ */
+export const createTicket = async data => {
+  const res = await axios.post(API, data);
   return res.data;
 };
 
+/* -----------------------------
+   Get single ticket by ID
+------------------------------ */
 export const getTicket = async id => {
   const res = await axios.get(`${API}/${id}`);
   return res.data;
 };
 
+/* -----------------------------
+   Reply to ticket
+------------------------------ */
 export const replyTicket = async (id, data) => {
   const res = await axios.post(`${API}/${id}/reply`, data);
   return res.data;
 };
 
+/* -----------------------------
+   Get all tickets (ADMIN)
+------------------------------ */
+export const getAllTickets = async () => {
+  const res = await axios.get(API);
+  return res.data;
+};
+
+/* -----------------------------
+   Close ticket (ADMIN)
+------------------------------ */
 export const closeTicket = async id => {
   const res = await axios.patch(`${API}/${id}/close`);
   return res.data;
