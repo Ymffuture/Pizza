80a940ba5e12a7d75e332fbc7a1a78719101e7b9
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-15 15:30:10 +0200

    Create sendEmail.js

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,21 @@
+import emailjs from "@emailjs/browser";
+
+export const sendEmail = async ({ email, ticketId, subject, message }) => {
+  try {
+    await emailjs.send(
+      "service_6kca9qq",
+      "template_uiudp97",
+      {
+        to_email: email,
+        ticket_id: ticketId,
+        subject,
+        message,
+      },
+      "lAEXMMHEtd0LxCc51"
+    );
+
+    console.log("✅ EmailJS email sent");
+  } catch (err) {
+    console.error("❌ EmailJS error:", err);
+  }
+};
