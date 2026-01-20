27129355851cb1028a638e855f042b7fbf795760
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-20 15:24:22 +0200

    Update AiLogin.jsx

--- src/layouts/AiLogin.jsx
+++ src/layouts/AiLogin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { supabase } from "../lib/supabaseClient";
+import { supabase } from "./lib/supabaseClient";
 import { toast } from "react-hot-toast";
 
 const AuthModal = ({ onClose, onLoginSuccess }) => {
