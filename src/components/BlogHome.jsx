bbfa48582d840b95df2bf61ad59ab5b7f5fedaa6
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-26 06:11:05 +0200

    Update BlogHome.jsx

--- src/components/BlogHome.jsx
+++ src/components/BlogHome.jsx
@@ -11,7 +11,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { FiThumbsUp } from "react-icons/fi";
 import { BsPatchCheckFill } from "react-icons/bs";
-
+import {Helmet} from "react-helmet" ;
 export default function BlogHome() {
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(1);
@@ -67,77 +67,12 @@ const [showViewModal, setShowViewModal] = useState(false);
       </button>
     </div>
   );
-const Loader = () => (
-  <div className="flex flex-col items-center justify-center min-h-[120px] bg-transparent overflow-hidden">
-    <svg
-  viewBox="0 0 100 100"
-  xmlns="http://www.w3.org/2000/svg"
-  width="64"
-  height="64"
-  role="progressbar"
-  aria-busy="true"
->
-  
-  <circle
-    cx="50"
-    cy="50"
-    r="40"
-    fill="none"
-    stroke="#1a73e8"
-    stroke-width="6"
-    stroke-linecap="round"
-    stroke-dasharray="1, 200"
-    stroke-dashoffset="0"
-  >
-    <animate
-      attributeName="stroke-dasharray"
-      values="1,200;90,200;1,200"
-      dur="1.5s"
-      repeatCount="indefinite"
-    />
-    <animate
-      attributeName="stroke-dashoffset"
-      values="0;-40;-120"
-      dur="1.5s"
-      repeatCount="indefinite"
-    />
-    <animateTransform
-      attributeName="transform"
-      type="rotate"
-      from="0 50 50"
-      to="360 50 50"
-      dur="2s"
-      repeatCount="indefinite"
-    />
-  </circle>
 
-  
-  <circle cx="50" cy="50" r="8" fill="#1a73e8">
-    <animate
-      attributeName="r"
-      values="8;12;8"
-      dur="1.5s"
-      repeatCount="indefinite"
-      keySplines="0.4 0 0.2 1"
-      calcMode="spline"
-    />
-    <animate
-      attributeName="opacity"
-      values="1;0.5;1"
-      dur="1.5s"
-      repeatCount="indefinite"
-    />
-  </circle>
-</svg>
-
-    <p className="text-gray-800 dark:text-gray-400 mt-3 text-sm tracking-wide">
-      Loading Blogs...
-    </p>
-  </div>
-);
-
+      
   
   return (
+
+    
     <div className="min-h-screen pb-28 md:pb-2 mx-auto px-4 pt-6 pb-4 max-w-2xl overflow-x-hidden">
 
       <InfiniteScroll
