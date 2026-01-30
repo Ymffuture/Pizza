7a0d81775079c7d1394d8f03f14f86521e64f0b5
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-01-30 14:00:48 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { CookieConsentProvider } from "./context/CookieConsentContext";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App className="dark:bg-white/5" />
+    <CookieConsentProvider>
+      <App />
+    </CookieConsentProvider>
   </StrictMode>,
 )
