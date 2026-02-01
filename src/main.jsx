99f63b4d4fc602e9f8b8605502f7093b4d45d17a
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-02-01 09:44:25 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { CookieConsentProvider } from "./context/CookieConsentContext";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queryClient";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      
+    
     <CookieConsentProvider>
       <App />
     </CookieConsentProvider>
+      </QueryClientProvider>
   </StrictMode>,
 )
