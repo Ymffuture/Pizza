9029975dbca69961b5498c974b58c2161eb27d47
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-02-01 09:15:40 +0200

    Create queryClient.js

---
+++ src/queryClient.js
@@ -0,0 +1,12 @@
+// queryClient.js
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 2, // 2 minutes
+      retry: 2,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
