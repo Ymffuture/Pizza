acd3667bbcd73ae150171cc3e22c98cc256cba98
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-02-09 11:24:32 +0200

    Update TrustedPartners.jsx

--- src/components/TrustedPartners.jsx
+++ src/components/TrustedPartners.jsx
@@ -11,7 +11,7 @@ import RenderLogo from "../assets/partners/emailjs_logo.png";
 import NodeLogo from "../assets/partners/wethinkcode.png";
 import GitHubLogo from "../assets/partners/GitHub.png";
 import VercelLogo from "../assets/partners/Vercel.png";
-import SupabaseLogo from "../assets/partners/supabase.png";
+import SupabaseLogo from "../assets/partners/supabase.jpg";
 import NetlifyLogo from "../assets/partners/Netlify.png";
 import TailwindLogo from "../assets/partners/Tailwindcss.png";
 
