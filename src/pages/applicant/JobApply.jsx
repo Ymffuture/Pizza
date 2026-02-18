da0e31f9419648bb3b261fa69043244e7dfb34dd
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2026-02-18 17:59:44 +0200

    Update JobApply.jsx

--- src/pages/applicant/JobApply.jsx
+++ src/pages/applicant/JobApply.jsx
@@ -63,7 +63,7 @@ export default function JobApply() {
         <h1 className="text-3xl font-semibold text-gray-900 dark:text-gray-100">
           Application (Developer / DoE) 
         </h1>
-         <Loader2 fullScreen/>
+         {loading? <Loader2 fullScreen/>:""} 
    <p className="text-black dark:text-white p-2 rounded-xl text-sm">This form will be in our system for 3 months for next application cycle </p>
 
 <p className="text-gray-600 dark:text-gray-300 p-2 rounded-xl text-sm">This form will act as Agreement between SwiftMeta and the applicant</p>
